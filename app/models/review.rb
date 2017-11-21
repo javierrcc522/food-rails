420bee8f2f0cbdeddc1f4b5a677ebbe72f0e4742
Javier Jimenez <javierrcc522@gmail.com>
2017-11-21 12:53:06 -0800

    add 6 passing validators

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,7 @@ class Review < ActiveRecord::Base
 
   validates :author, :presence => true
   validates :rating, :inclusion => {:in => [1,5]}
-
-
+  
   validates_length_of :content_body, :maximum => 250
   validates_length_of :content_body, :minimum => 50
 end
