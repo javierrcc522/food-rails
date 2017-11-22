369031403bb0a1a6866d7ee04a05a128aa43e807
Javier Jimenez <javierrcc522@gmail.com>
2017-11-22 14:36:10 -0800

    add 3rd capybera test

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -6,7 +6,7 @@ describe "the add a product process" do
     visit products_path
     click_link 'Add a New Product'
     fill_in 'Name', :with => 'Apples'
-    fill_in 'Cost', :with => "3"
+    fill_in 'Cost', :with => '3'
     fill_in 'Country origin', :with => 'America'
     click_on 'Create Product'
     expect(page).to have_content 'Products'
--- spec/features/edit_review_pages_spec.rb
+++ spec/features/edit_review_pages_spec.rb
@@ -1,1 +1,17 @@
+require 'rails_helper'
 
+describe "the edit a review process" do
+  it "edits a review" do
+    product = Product.create(:name => "product", :cost => "1", :country_origin => "usa" )
+    review_one = Review.create(:author => 'review', :rating => '1', :content_body => 'qwqwqwqawerwaewerqqqwerqweqrqrqrwqrqwerwqrqwerqwerq', :product_id => product.id)
+    visit product_path(product)
+    click_link 'Edit'
+    fill_in 'Author', :with => 'Chanchela'
+    fill_in 'Content body', :with => 'qwqwqwqawerwaewerqqqwerqweqrqrqrwqrqwerwqrqwerqwerq'
+    fill_in 'Rating', :with => '3'
+    click_on 'Update Review'
+    expect(page).to have_content 'Chanchela', 'qwqwqwqawerwaewerqqqwerqweqrqrqrwqrqwerwqrqwerqwerq', '3'
+  end
+
+
+end
