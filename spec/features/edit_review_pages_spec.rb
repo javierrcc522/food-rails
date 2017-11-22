3a5aba5c4aeca1759c60e407c0d4985af243c602
Javier Jimenez <javierrcc522@gmail.com>
2017-11-22 15:19:33 -0800

    add 3 integrated test author, content body and rating

--- spec/features/edit_review_pages_spec.rb
+++ spec/features/edit_review_pages_spec.rb
@@ -13,5 +13,33 @@ describe "the edit a review process" do
     expect(page).to have_content 'Chanchela', 'qwqwqwqawerwaewerqqqwerqweqrqrqrwqrqwerwqrqwerqwerq', '3'
   end
 
+  it "gives error when no author is entered " do
+    product = Product.create(:name => "product", :cost => "1", :country_origin => "usa" )
+    review_one = Review.create(:author => 'review', :rating => '1', :content_body => 'qwqwqwqawerwaewerqqqwerqweqrqrqrwqrqwerwqrqwerqwerq', :product_id => product.id)
+    visit product_path(product)
+    click_link 'Edit'
+    fill_in 'Author', :with => ''
+    click_on 'Update Review'
+    expect(page).to have_content "Author can't be blank"
+  end
 
+  it "gives error when no rating is entered " do
+    product = Product.create(:name => "product", :cost => "1", :country_origin => "usa" )
+    review_one = Review.create(:author => 'review', :rating => '1', :content_body => 'qwqwqwqawerwaewerqqqwerqweqrqrqrwqrqwerwqrqwerqwerq', :product_id => product.id)
+    visit product_path(product)
+    click_link 'Edit'
+    fill_in 'Rating', :with => ''
+    click_on 'Update Review'
+    expect(page).to have_content "Rating is not included in the list"
+  end
+
+  it "gives error when no content body is entered " do
+    product = Product.create(:name => "product", :cost => "1", :country_origin => "usa" )
+    review_one = Review.create(:author => 'review', :rating => '1', :content_body => 'qwqwqwqawerwaewerqqqwerqweqrqrqrwqrqwerwqrqwerqwerq', :product_id => product.id)
+    visit product_path(product)
+    click_link 'Edit'
+    fill_in 'Content body', :with => ''
+    click_on 'Update Review'
+    expect(page).to have_content "Content body is too short (minimum is 50 characters)"
+  end
 end
